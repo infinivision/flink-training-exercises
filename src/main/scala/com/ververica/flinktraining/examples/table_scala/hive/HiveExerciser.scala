8ebabe8cf7e23b2a099f9212c5e28973d6163cbc
Hongtao Zhang <553780043@qq.com>
2019-08-29 18:27:52 +0800

    add hive example

--- src/main/scala/com/ververica/flinktraining/examples/table_scala/hive/HiveExerciser.scala
+++ src/main/scala/com/ververica/flinktraining/examples/table_scala/hive/HiveExerciser.scala
@@ -1,24 +1,32 @@
 package com.ververica.flinktraining.examples.table_scala.hive
 
-import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.table.api.scala.StreamTableEnvironment
+import org.apache.flink.table.api.config.ExecutionConfigOptions.TABLE_EXEC_RESOURCE_DEFAULT_PARALLELISM
 import org.apache.flink.table.catalog.hive.HiveCatalog
+import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
+import org.apache.flink.table.api._
+import org.apache.flink.api._
 
 object HiveExerciser {
-  // set up streaming execution environment
-  val env = StreamExecutionEnvironment.getExecutionEnvironment
-  env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
-  // create TableEnvironment
-  val tEnv = StreamTableEnvironment.create(env)
-  val hiveCatalog = new HiveCatalog("infinivision_hive", "infinivision_cdp",
-    "/etc/hadoop/conf", "1.2.1")
+  def main(args: Array[String]): Unit = {
 
-  tEnv.registerCatalog("infinivision_hive", hiveCatalog)
-  tEnv.useCatalog("infinivision_hive")
-  println(s"CataLogs: ${tEnv.listCatalogs()}")
-  println(s"Tables: ${tEnv.listTables()}")
+    // create Blink Planner Table Environment
+    val settings = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build()
+    val tEnv = TableEnvironment.create(settings)
+    tEnv.getConfig.getConfiguration.setInteger(TABLE_EXEC_RESOURCE_DEFAULT_PARALLELISM.key, 3)
 
-  env.execute("Flink-1.9 Hive Table Read Testing")
+    //    val tEnv = StreamTableEnvironment.create(env, settings)
+    val hiveCatalog = new HiveCatalog("infinivision_hive", "infinivision_cdp",
+      "/etc/hadoop/conf", "1.2.1")
+
+    tEnv.registerCatalog("infinivision_hive", hiveCatalog)
+    tEnv.useCatalog("infinivision_hive")
+    println(s"CataLogs: ${tEnv.listCatalogs()}")
+    println(s"Tables: ${tEnv.listTables()}")
+
+//    val t = tEnv.sqlQuery("SELECT COUNT(*) FROM cdp_campaign_mid_tag_ordered")
+
+    tEnv.execute("Flink-1.9 Hive Table Read Testing")
+  }
+
 }
